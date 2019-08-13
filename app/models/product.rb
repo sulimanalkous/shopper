b9fef7bca91045555768cbba946c84419c2eb7d6
suliman <sulimanalkous@gmail.com>
2019-08-14 02:51:24 +0300

    finish part 2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  validates :title, presence: true
+
+  has_many :product_categories
+  has_many :categories, through: :product_categories
+
+  has_many :variants, class_name: 'ProductVariant'
 end
